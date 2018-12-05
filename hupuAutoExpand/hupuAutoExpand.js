11cc6850ee81c4dea2b68b7db875b77ff42f0267
zz <zy13474176527@gmail.com>
2018-12-05 15:08:57 +0800

    hupu v0.3

--- hupuAutoExpand/hupuAutoExpand.js
+++ hupuAutoExpand/hupuAutoExpand.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         虎扑引用楼层内容过长自动折叠
 // @namespace    http://tampermonkey.net/
-// @version      0.2
-// @description  虎扑引用楼层内容过长自动折叠，增加展开和收起按钮
+// @version      0.3
+// @description  虎扑引用楼层内容过长自动折叠，v0.2增加展开和收起按钮，v0.3超过一定长度才会折叠
 // @author       zerozz
 // @match        https://bbs.hupu.com/*.html
 // @grant        none
@@ -21,34 +21,33 @@
             this.parentNode.previousElementSibling.style.maxHeight = "100px";
             this.innerText = "展开";
         }
-        //this.previousElementSibling.style.cssText='max-height: none; overflow: hidden; transition: max-height 2s ease 0s;';
-        console.log(this);
-        // this.parentNode.removeChild(this);
     }
 
     var bqEleArr = document.getElementsByTagName("blockquote") || [];
     var size = bqEleArr.length;
     for (var i = 0; i < size; i++) {
-        var bqEle = bqEleArr[i];
-        bqEle.style.cssText='max-height: 100px; overflow: hidden; transition: max-height 2s ease 0s;';
+		if (bqEle.offsetHeight > 200) {
+			var bqEle = bqEleArr[i];
+			bqEle.style.cssText='max-height: 100px; overflow: hidden; transition: max-height 2s ease 0s;';
 
-        var expandParentEle = document.createElement("div");
-        expandParentEle.id = "expandParentEle" + i;
-        expandParentEle.style.cssText = "text-align: center;padding: 10px;";
+			var expandParentEle = document.createElement("div");
+			expandParentEle.id = "expandParentEle" + i;
+			expandParentEle.style.cssText = "text-align: center;padding: 10px;";
 
-        var expandEle = document.createElement("span");
-        expandEle.id = "expandBtn" + i;
-        expandEle.style.cssText = "color:#108089;cursor: pointer;padding: 4px 7px;border-radius: 3px;border-width: 1px;border-style: solid;border-color: rgb(166, 166, 166);transition: all 0.1s ease 0s;";
-        expandEle.innerText = "展开";
-        expandEle.onclick = onClickExpand;
-        expandParentEle.appendChild(expandEle);
+			var expandEle = document.createElement("span");
+			expandEle.id = "expandBtn" + i;
+			expandEle.style.cssText = "color:#108089;cursor: pointer;padding: 4px 7px;border-radius: 3px;border-width: 1px;border-style: solid;border-color: rgb(166, 166, 166);transition: all 0.1s ease 0s;";
+			expandEle.innerText = "展开";
+			expandEle.onclick = onClickExpand;
+			expandParentEle.appendChild(expandEle);
 
-        var parent = bqEle.parentNode;
-        if (parent.lastChild == bqEle) {
-            parent.appendChild(expandParentEle);
-        } else {
-            parent.insertBefore(expandParentEle, bqEle.nextSibling)
-        }
+			var parent = bqEle.parentNode;
+			if (parent.lastChild == bqEle) {
+				parent.appendChild(expandParentEle);
+			} else {
+				parent.insertBefore(expandParentEle, bqEle.nextSibling)
+			}
+		}
     }
 
 
