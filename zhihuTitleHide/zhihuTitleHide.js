ddce46c2a01f880328e61bbc73562f362515fe30
曾峥 <zengzheng@hzwanglan.com>
2020-12-28 17:34:40 +0800

    去除知乎最上方标题栏

---
+++ zhihuTitleHide/zhihuTitleHide.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         去除知乎最上方标题栏
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  去除知乎最上方标题栏，右边的侧边栏，设置正文宽度为显示宽度的2/3
+// @author       zz
+// @match        https://www.zhihu.com/question/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    setTimeout(function() {
+        document.getElementsByClassName("PageHeader")[0].remove();
+        document.getElementsByClassName("AppHeader-inner")[0].remove();
+        let stickyArr = document.getElementsByClassName("Sticky");
+        let stickySize = stickyArr.length;
+        for (let i = 0; i < stickySize; i++) {
+           stickyArr[i] ? stickyArr[i].remove() : null;
+        }
+
+        setTimeout(function() {
+            document.getElementsByClassName("QuestionAnswers-answers")[0].style.width = document.body.clientWidth / 3 * 2 + 'px';
+        }, 100);
+    }, 500);
+})();
